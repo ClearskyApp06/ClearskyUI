8c719e61aa92257d72f0ef3e10158135cd297984
ParmesanPapi17 <adammolina17@gmail.com>
2024-12-25 11:17:03 -0500

    added Relative Time Format to handle-history file

--- src/detail-panels/account-header/handle-history/handle-history.jsx
+++ src/detail-panels/account-header/handle-history/handle-history.jsx
@@ -20,26 +20,31 @@ export function HandleHistory({ handleHistory }) {
   let lastHandle;
   /** @type {string | undefined} */
   let lastPds;
-  const entries = handleHistory.slice().reverse().map(([handle, date, pds], i) => {
-    if (!handle) handle = lastHandle || '';
-    if (!pds) pds = lastPds || '';
-    const isCreated = !i;
+  const entries = handleHistory
+    .slice()
+    .reverse()
+    .map(([handle, date, pds], i) => {
+      if (!handle) handle = lastHandle || '';
+      if (!pds) pds = lastPds || '';
+      const isCreated = !i;
 
-    const entry = (
-      <ChangeEntry key={i}
-        date={date}
-        handle={handle}
-        pds={pds}
-        lastHandle={lastHandle}
-        lastPds={lastPds}
-        isCreated={isCreated} />
-    );
+      const entry = (
+        <ChangeEntry
+          key={i}
+          date={date}
+          handle={handle}
+          pds={pds}
+          lastHandle={lastHandle}
+          lastPds={lastPds}
+          isCreated={isCreated}
+        />
+      );
 
-    lastHandle = handle;
-    lastPds = pds;
+      lastHandle = handle;
+      lastPds = pds;
 
-    return entry;
-  });
+      return entry;
+    });
 
   return <>{entries}</>;
 }
@@ -60,79 +65,101 @@ function ChangeEntry({ date, handle, pds, lastHandle, lastPds, isCreated }) {
   const isMigratedToMushroom =
     isPdsChanged &&
     pds.endsWith('bsky.network') &&
-    lastPds && lastPds.endsWith('bsky.social');
-  const isMigratedExternally =
-    isPdsChanged &&
-    !pds.endsWith('bsky.network');
-  
-  if (isCreated) return (
-    <div className='event-entry'>
-      {date ? <FormatTimestamp className='event-timestamp' timestamp={date} /> : 'unknown date '}
-      <span className='event-label event-label-registered'>
-        {localise('registered ', { uk: 'зареєстровано ' })}
-      </span>
-      {
-        !handle ? undefined :
+    lastPds &&
+    lastPds.endsWith('bsky.social');
+  const isMigratedExternally = isPdsChanged && !pds.endsWith('bsky.network');
+
+  // getting elapsed time
+  function getRelativeTime(dateString) {
+    const newDate = new Date(dateString); //converts sring to Date obj
+    const now = new Date();
+    const timeDiffInMillis = now.getTime() - newDate.getTime();
+    const daysDiff = Math.round(timeDiffInMillis / (1000 * 60 * 60 * 24));
+
+    const inferred = new Intl.RelativeTimeFormat(undefined, {
+      numeric: 'auto',
+    });
+    return inferred.format(daysDiff, 'day');
+  }
+
+  if (isCreated)
+    return (
+      <div className="event-entry">
+        {date ? getRelativeTime(date) : 'unknown date '}
+        <span className="event-label event-label-registered">
+          {localise('registered ', { uk: 'зареєстровано ' })}
+        </span>
+        {!handle ? undefined : (
           <>
             {localise('as ', { uk: 'як ' })}
             <AtAndHandle shortHandle={handle} />
           </>
-      }
-      {
-        !pds ? undefined :
+        )}
+        {!pds ? undefined : (
           <>
             {localise(' at ', { uk: ' на ' })}
             <PDSName pds={pds} />
           </>
-      }
-    </div>
-  );
+        )}
+      </div>
+    );
 
-  if (!isHandleChanged && !isPdsChanged) return (
-    <div className='event-entry'>
-      {date ? <FormatTimestamp className='event-timestamp' timestamp={date} /> : 'unknown date'}
-      <span className='event-label event-label-handle-changed'>
-        {localise('data refresh', { uk: 'оновлення' })}
-      </span>
-    </div>
-  );
+  if (!isHandleChanged && !isPdsChanged)
+    return (
+      <div className="event-entry">
+        {date ? (
+          <FormatTimestamp className="event-timestamp" timestamp={date} />
+        ) : (
+          'unknown date'
+        )}
+        <span className="event-label event-label-handle-changed">
+          {localise('data refresh', { uk: 'оновлення' })}
+        </span>
+      </div>
+    );
 
   const handlePart = !isHandleChanged ? undefined : (
     <>
-      <span className='event-label event-label-handle-changed'>
+      <span className="event-label event-label-handle-changed">
         {localise('changed handle to ', { uk: 'псевдоним ' })}
       </span>
       <AtAndHandle shortHandle={handle} />
     </>
   );
 
-  const pdsPart = !isPdsChanged ? undefined : (
-    isMigratedToMushroom ?
-      <>
-        <span className='event-label event-label-pds-changed event-label-pds-migrated-to-mushroom'>
-          {localise('mushroom migration ', { uk: 'переселення до грибів ' })}
-        </span>
-        <PDSName pds={pds} />
-      </> :
-      isMigratedExternally ?
-        <>
-          <span className='event-label event-label-pds-changed event-label-pds-migrated-externally'>
-            {localise('to external server ', { uk: 'на незалежний сервер ' })}
-          </span>
-          <PDSName pds={pds} />
-        </> :
-        <>
-        <span className='event-label event-label-pds-changed'>
-          {localise('server migrated ', { uk: 'на інший сервер ' })}
-          </span>
-          <PDSName pds={pds} />
-        </>
+  const pdsPart = !isPdsChanged ? undefined : isMigratedToMushroom ? (
+    <>
+      <span className="event-label event-label-pds-changed event-label-pds-migrated-to-mushroom">
+        {localise('mushroom migration ', { uk: 'переселення до грибів ' })}
+      </span>
+      <PDSName pds={pds} />
+    </>
+  ) : isMigratedExternally ? (
+    <>
+      <span className="event-label event-label-pds-changed event-label-pds-migrated-externally">
+        {localise('to external server ', { uk: 'на незалежний сервер ' })}
+      </span>
+      <PDSName pds={pds} />
+    </>
+  ) : (
+    <>
+      <span className="event-label event-label-pds-changed">
+        {localise('server migrated ', { uk: 'на інший сервер ' })}
+      </span>
+      <PDSName pds={pds} />
+    </>
   );
 
   return (
-    <div className='event-entry'>
-        {date ? <FormatTimestamp className='event-timestamp' timestamp={date} /> : 'unknown date'}
-      {handlePart}{handlePart && pdsPart ? ' ' : undefined}{pdsPart}
+    <div className="event-entry">
+      {date ? (
+        <FormatTimestamp className="event-timestamp" timestamp={date} />
+      ) : (
+        'unknown date'
+      )}
+      {handlePart}
+      {handlePart && pdsPart ? ' ' : undefined}
+      {pdsPart}
     </div>
   );
 }
@@ -145,12 +172,18 @@ function ChangeEntry({ date, handle, pds, lastHandle, lastPds, isCreated }) {
  */
 function AtAndHandle({ className, shortHandle }) {
   const handleAt =
-    shortenHandle(shortHandle).indexOf('.') < 0 ? <span className='handle-at-sign'>@</span> :
-      <img className='handle-domain-icon' src={'https://icon.horse/icon/' + unwrapShortHandle(shortHandle)} />
+    shortenHandle(shortHandle).indexOf('.') < 0 ? (
+      <span className="handle-at-sign">@</span>
+    ) : (
+      <img
+        className="handle-domain-icon"
+        src={'https://icon.horse/icon/' + unwrapShortHandle(shortHandle)}
+      />
+    );
   return (
     <span className={'handle-history-handle ' + (className || '')}>
       {handleAt}
-      <FullHandle className='handle-history-handle' shortHandle={shortHandle} />
+      <FullHandle className="handle-history-handle" shortHandle={shortHandle} />
     </span>
   );
 }
