291a82402a5347194eef50dc1239a743199de544
Sean Munson <sean_munson@yahoo.com>
2025-01-25 02:44:02 -0500

    add anchor

---
+++ src/common-components/conditional-anchor.jsx
@@ -0,0 +1,38 @@
+import { jsx } from "@emotion/react";
+import React from "react";
+
+/**
+ * @param {{
+*  target:string,
+*  href: string,
+*  condition: boolean,
+*  className?: string,
+*  children?:React.ReactNode,
+* }} 
+*/
+
+export function ConditionalAnchor(props){
+
+    const {target,
+        href,
+        condition,
+        className,
+        children} = props;
+    
+    if (condition){
+        return(
+            <a href={href} target={target} className={className}>
+                {children}
+            </a>
+        )
+    }else{
+            <>
+                {children}
+            </>
+    }
+
+
+
+
+
+}
