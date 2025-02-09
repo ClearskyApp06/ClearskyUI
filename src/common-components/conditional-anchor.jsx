e1e5576266517e6f77bcd6e883e2900e1fd94595
Noah Manneschmidt <noah@manneschmidt.net>
2025-02-09 15:25:38 -0800

    stop logging from conditional anchor

--- src/common-components/conditional-anchor.jsx
+++ src/common-components/conditional-anchor.jsx
@@ -1,42 +1,24 @@
-import { jsx } from "@emotion/react";
-import React from "react";
+import React from 'react';
 
 /**
  * @param {{
-*  target:string,
-*  href: string,
-*  condition: boolean,
-*  className?: string,
-*  children?:React.ReactNode,
-*  style?:CSSProperties
-* }} 
-*/
-
-export function ConditionalAnchor(props){
-
-    const {target,
-        href,
-        condition,
-        className,
-        children,
-        style,} = props;
-    console.log(condition)
-    if (condition){
-        return(
-            <a href={href} target={target} className={className} style={style}>
-                {children}
-            </a>
-        )
-    }else{
-        return(
-            <span >
-                {children}
-            </span>
-        );
-    }
-
-
-
-
-
+ *  target:string,
+ *  href: string,
+ *  condition: boolean,
+ *  className?: string,
+ *  children?: React.ReactNode,
+ *  style?: CSSProperties
+ * }}
+ */
+export function ConditionalAnchor(props) {
+  const { target, href, condition, className, children, style } = props;
+  if (condition) {
+    return (
+      <a href={href} target={target} className={className} style={style}>
+        {children}
+      </a>
+    );
+  } else {
+    return <span>{children}</span>;
+  }
 }
