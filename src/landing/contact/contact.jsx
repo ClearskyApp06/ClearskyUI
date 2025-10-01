43280d8719378dbe5db66f2e5f76dcdf320fb962
Justin <justin@thieflord.dev>
2025-10-01 12:54:43 -0500

    Fix contractions in success messages
    Signed-off-by: Justin <justin@thieflord.dev>

--- src/landing/contact/contact.jsx
+++ src/landing/contact/contact.jsx
@@ -175,7 +175,7 @@ export default function Contact() {
       if (response && response.status === 'success') {
         setSubmitStatus({
           type: 'success',
-          message: 'Your message has been sent successfully. We\'ll get back to you soon!'
+          message: 'Your message has been sent successfully. We will get back to you soon!'
         });
         
         // Clear form and errors after successful submission
@@ -227,7 +227,7 @@ export default function Contact() {
               Contact Us
             </Typography>
             <Typography variant="body1" color="text.secondary">
-              Send us a message and we'll get back to you as soon as possible.
+              Send us a message and we will get back to you as soon as possible.
             </Typography>
           </Box>
 
