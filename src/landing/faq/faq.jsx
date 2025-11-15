d87721af9f44efee698f1de1c3e13f24ef6b1e5e
PPadilla44 <Padilla.pablo1998@gmail.com>
2025-11-14 17:41:01 -0800

    Added MD support

--- src/landing/faq/faq.jsx
+++ src/landing/faq/faq.jsx
@@ -9,9 +9,10 @@ import {
   Button,
 } from '@mui/material';
 import { useFaq } from '../../api/faq';
-import { AssignmentOutlined, CloseOutlined } from '@mui/icons-material';
+import { AssignmentOutlined, Add } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { HydrateFallback } from '../../common-components/hydrate-fallback';
+import ReactMarkdown from 'react-markdown';
 
 function AccordionIcon({ expanded }) {
   return (
@@ -20,10 +21,10 @@ function AccordionIcon({ expanded }) {
         fontWeight: 'bold',
         fontSize: 20,
         transition: 'transform 0.3s',
-        transform: expanded ? 'rotate(0deg)' : 'rotate(45deg)',
+        transform: expanded ? 'rotate(45deg)' : 'rotate(0deg)',
       }}
     >
-      <CloseOutlined />
+      <Add />
     </Box>
   );
 }
@@ -47,68 +48,100 @@ export default function FAQ() {
   }, {});
 
   return (
-    <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
-      <Box sx={{ textAlign: 'center' }}>
-        <Button onClick={() => navigate('/')} sx={{ color: 'gray' }}>
-          ← Back to Home
-        </Button>
-      </Box>
-      <Typography variant="h3" textAlign="center" gutterBottom>
-        FAQ
-      </Typography>
-
-      {
-        error && <Typography textAlign="center" color="error">{error.message}</Typography>
-      }
-
-      {Object.entries(faqsByCategory).map(([category, faqsInCategory]) => (
-        <Box key={category} mb={3}>
-          <Box display="flex" alignItems="center" mb={1}>
-            <Typography
-              variant="h6"
-              textTransform="capitalize"
-              fontWeight="bold"
-              sx={{ flexGrow: 1 }}
-            >
-              {category}
-            </Typography>
-          </Box>
-
-          {faqsInCategory.map((faq, index) => {
-            const id = `${category}-${faq.question.substring(0, 20)}-${index}`
-            return (
-              <Accordion
-                key={id}
-                expanded={expandedId === id}
-                onChange={handleChange(id)}
-                sx={{ mb: 1 }}
-              >
-                <AccordionSummary
-                  expandIcon={<AccordionIcon expanded={expandedId === id} />}
-                >
-                  <AssignmentOutlined sx={{ mr: 1 }} color="info" />
-                  <Typography
-                    variant="subtitle1"
-                    fontWeight="bold"
-                    color="text.primary"
-                  >
-                    {faq.question}
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography
-                    variant="body1"
-                    fontWeight="regular"
-                    color="text.secondary"
-                  >
-                    {faq.answer}
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-            );
-          })}
+    <Box
+      sx={{
+        paddingY: 6,
+        height: '100dvh',
+        overflowY: 'auto',
+      }}
+    >
+      <Container maxWidth="md">
+        <Box sx={{ textAlign: 'center' }}>
+          <Button onClick={() => navigate('/')} sx={{ color: 'gray' }}>
+            ← Back to Home
+          </Button>
         </Box>
-      ))}
-    </Container>
+        <Typography variant="h3" textAlign="center" gutterBottom>
+          FAQ
+        </Typography>
+
+        {error && (
+          <Typography textAlign="center" color="error">
+            {error.message}
+          </Typography>
+        )}
+
+        {Object.entries(faqsByCategory).map(([category, faqsInCategory]) => (
+          <Box key={category} mb={3}>
+            <Box display="flex" alignItems="center" mb={1}>
+              <Typography
+                variant="h6"
+                textTransform="capitalize"
+                fontWeight="bold"
+                sx={{ flexGrow: 1 }}
+              >
+                {category}
+              </Typography>
+            </Box>
+
+            {faqsInCategory.map((faq, index) => {
+              const id = `${category}-${faq.question.substring(0, 20)}-${index}`;
+              return (
+                <Accordion
+                  key={id}
+                  expanded={expandedId === id}
+                  onChange={handleChange(id)}
+                  sx={{ mb: 1 }}
+                >
+                  <AccordionSummary
+                    expandIcon={<AccordionIcon expanded={expandedId === id} />}
+                    sx={{
+                      '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+                        transform: 'none !important',
+                      },
+                    }}
+                  >
+                    <AssignmentOutlined sx={{ mr: 1 }} color="info" />
+                    <Typography variant="subtitle1" fontWeight="bold">
+                      {faq.question}
+                    </Typography>
+                  </AccordionSummary>
+
+                  <AccordionDetails>
+                    <ReactMarkdown
+                      components={{
+                        p: (props) => (
+                          <Typography
+                            variant="body1"
+                            fontWeight="regular"
+                            color="text.secondary"
+                            paragraph
+                            {...props}
+                          />
+                        ),
+                        strong: (props) => (
+                          <Typography component="strong" fontWeight="bold" {...props} />
+                        ),
+                        li: (props) => (
+                          <li>
+                            <Typography
+                              variant="body1"
+                              color="text.secondary"
+                              {...props}
+                            />
+                          </li>
+                        ),
+                      }}
+                    >
+                      {faq.answer}
+                    </ReactMarkdown>
+                  </AccordionDetails>
+                </Accordion>
+              );
+            })}
+          </Box>
+        ))}
+      </Container>
+    </Box>
   );
 }
