e8096e529a5e9b17193527a8ca5f0b51ca1c7d15
Sumanth Reddy <sumanthcob@gmail.com>
2026-01-14 10:56:22 -0500

    refactor: code cleanup

--- src/detail-panels/blocking/index.jsx
+++ src/detail-panels/blocking/index.jsx
@@ -3,7 +3,7 @@
 import { useBlocklist } from '../../api';
 import { BlockPanelGeneric } from '../block-panel-generic';
 import { localise } from '../../localisation';
-import { useBlocklistCount, useIsBlocking } from '../../api/blocklist';
+import { useBlocklistCount } from '../../api/blocklist';
 import { useAccountResolver } from '../account-resolver';
 
 export default function BlockingPanel() {
@@ -11,9 +11,6 @@ export default function BlockingPanel() {
   const did = accountQuery.data?.shortDID;
   const blocklistQuery = useBlocklist(did);
   const totalQuery = useBlocklistCount(did);
-
-  // console.log("/blocklist-search-blocking NON WORKING EXAMPLE 'georgetakei', 'bradtakeii'", useIsBlocking('georgetakei', 'bradtakeii').data); // Example usage
-  console.log("/blocklist-search-blocking WORKING EXAMPLE 'chiefkeef', 'lovehyphen'", useIsBlocking('chiefkeef', 'lovehyphen').data); // Example usage
 
   return (
     <BlockPanelGeneric
